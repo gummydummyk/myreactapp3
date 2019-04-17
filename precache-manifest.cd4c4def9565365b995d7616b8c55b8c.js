b2e65f137876011b4ec4eaee1f831051f37a3b9e
chanel <chanel@nn.com>
2019-04-17 15:10:56 +0800

    Updates

--- precache-manifest.cd4c4def9565365b995d7616b8c55b8c.js
+++ precache-manifest.cd4c4def9565365b995d7616b8c55b8c.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/myreactapp3/static/js/runtime~main.f0517f2e.js"
   },
   {
-    "revision": "e0c627f285bddd3f7163",
-    "url": "/myreactapp3/static/js/main.1df80275.chunk.js"
+    "revision": "ecabb0eb30cf3f25841a",
+    "url": "/myreactapp3/static/js/main.5ea91e7f.chunk.js"
   },
   {
     "revision": "443637cb4c037aa15ccc",
     "url": "/myreactapp3/static/js/2.1ed43d11.chunk.js"
   },
   {
-    "revision": "e0c627f285bddd3f7163",
+    "revision": "ecabb0eb30cf3f25841a",
     "url": "/myreactapp3/static/css/main.08ca32c5.chunk.css"
   },
   {
-    "revision": "ef79aeb8e8a34abe7382077c5f2fde3b",
+    "revision": "5b3a378a80d90d0c6917e3bb62520dd5",
     "url": "/myreactapp3/index.html"
   }
 ];
