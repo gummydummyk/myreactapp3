2026de262e7ae67d63d9253074b1ac21fde8eeb4
chanel <chanel@nn.com>
2019-04-17 14:45:20 +0800

    Add API request

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
+const url = process.env.NODE_ENV !== 'production' ? '/react-intl-2/src/Data.php' : './Data.php';
+
 class Homepage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(`${url}`, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    })
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
   render() {
+    console.log(process.env.NODE_ENV);
     return (
       <div>
         <div className="container">
-          <h1>Home</h1>
+          <h1>Home {this.state.data}</h1>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
