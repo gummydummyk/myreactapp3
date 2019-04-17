1090fc6f4aa4deb4444a428337c220456fd2dcae
chanel <chanel@nn.com>
2019-04-17 13:34:16 +0800

    Updates

---
+++ precache-manifest.c85c51c26370ea2ddd37f4c97828b64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactapp3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0bfd466c2319b3b76f81",
+    "url": "/myreactapp3/static/js/runtime~main.f0517f2e.js"
+  },
+  {
+    "revision": "492b7dce6586c0308f6c",
+    "url": "/myreactapp3/static/js/main.8801932b.chunk.js"
+  },
+  {
+    "revision": "443637cb4c037aa15ccc",
+    "url": "/myreactapp3/static/js/2.1ed43d11.chunk.js"
+  },
+  {
+    "revision": "492b7dce6586c0308f6c",
+    "url": "/myreactapp3/static/css/main.0fbc8c36.chunk.css"
+  },
+  {
+    "revision": "26f17c5651d99503bc23f7b30e7c368f",
+    "url": "/myreactapp3/index.html"
+  }
+];
