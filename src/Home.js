4cd1406cb9601128db1366b6595eb133d1dc2605
chanel <chanel@nn.com>
2019-04-17 14:53:36 +0800

    Remove API request

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 
-const url = process.env.NODE_ENV !== 'production' ? '/react-intl-2/src/Data.php' : './Data.php';
+//const url = process.env.NODE_ENV !== 'production' ? '/react-intl-2/src/Data.php' : './Data.php';
 
 class Homepage extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      data: null,
-    };
-  }
+  //   this.state = {
+  //     data: null,
+  //   };
+  // }
 
-  componentDidMount() {
-    fetch(`${url}`, {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
+  // componentDidMount() {
+  //   fetch(`${url}`, {
+  //     headers : { 
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //      }
 
-    })
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
-  }
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ data }));
+  // }
 
   render() {
     console.log(process.env.NODE_ENV);
     return (
       <div>
         <div className="container">
-          <h1>Home {this.state.data}</h1>
+          {/* {this.state.data} */}
+          <h1>Home</h1>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
