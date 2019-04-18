2ebf6d053b85b2ce2bd013dd41d520a123136746
chanel <chanel@nn.com>
2019-04-18 15:08:01 +0800

    Add reactstrap

--- src/includes/header.js
+++ src/includes/header.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem, Container } from 'reactstrap';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+    }
+
     render() {
         return(
             <div>
-                <nav className="navbar">
+                <Navbar light expand="md">
+                    <Container>
+                    <NavbarBrand href="/">Home</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to="/about">About</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                            Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                            <DropdownItem>
+                                Option 1
+                            </DropdownItem>
+                            <DropdownItem>
+                                Option 2
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem>
+                                Reset
+                            </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                    </Container>
+                </Navbar>
+                {/* <nav className="navbar">
                     <div className="container">
                         <ul>
                             <li><NavLink exact to="/">Home</NavLink></li>
@@ -13,7 +72,7 @@ class Header extends Component {
                             <li><NavLink to="/about">About</NavLink></li>
                         </ul>
                     </div>
-                </nav>
+                </nav> */}
             </div>
         );
     }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DivStyled from './style';
+import { Container, Row, Col } from 'reactstrap';
 
 class Homepage extends Component {
   render() {
@@ -11,6 +12,16 @@ class Homepage extends Component {
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
             <button>Click Me</button>
           </div>
+        </DivStyled>
+        <DivStyled>
+          <Container>
+            <Row>
+              <Col xs="8" sm={{ size: 8, offset: 2 }}>
+                <h1>Home with default style</h1>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+              </Col>
+            </Row>
+          </Container>
         </DivStyled>
         <DivStyled primary round>
           <div className="container">
